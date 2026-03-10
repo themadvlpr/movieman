0baaee8f18cdf8c1a171100204d4f2a610d115a1
themadvlpr <themadvlpr@gmail.com>
2026-03-10 19:34:42 +0200

    set better-auth up

---
+++ src/hooks/use-auth-actions.ts
@@ -0,0 +1,62 @@
+'use client'
+
+import { authClient } from '@/lib/auth-client'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+export function useAuthActions() {
+	const router = useRouter()
+	const [loadingType, setLoadingType] = useState<'github' | 'google' | null>(
+		null,
+	)
+	const signInWithGithub = async () => {
+		setLoadingType('github')
+		try {
+			await authClient.signIn.social({
+				provider: 'github',
+				callbackURL: window.location.href,
+			})
+		} catch (error) {
+			setLoadingType(null)
+			console.error(error)
+		}
+	}
+
+	const signInWithGoogle = async () => {
+		setLoadingType('google')
+		try {
+			await authClient.signIn.social({
+				provider: 'google',
+				callbackURL: window.location.href,
+				fetchOptions: {
+					query: {
+						prompt: 'select_account',
+					},
+				},
+			})
+		} catch (error) {
+			console.error(error)
+		} finally {
+			setLoadingType(null)
+		}
+	}
+
+	const signOut = async () => {
+		setLoadingType('github')
+		await authClient.signOut({
+			fetchOptions: {
+				onSuccess: () => {
+					router.refresh()
+				},
+			},
+		})
+		setLoadingType(null)
+	}
+
+	return {
+		signInWithGithub,
+		signInWithGoogle,
+		signOut,
+		loadingType,
+	}
+}
