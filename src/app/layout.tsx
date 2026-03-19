3299f7cf0e1fac63d94b6fed749ba3d95b35106b
themadvlpr <themadvlpr@gmail.com>
2026-03-19 14:28:58 +0200

    added new icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,17 @@ const montserrat = Montserrat({
 export const metadata: Metadata = {
 	title: 'MovieMan',
 	description: 'Search, collect and share your movie collection',
+	icons: {
+		icon: [
+			{ url: '/icons/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+			{ url: '/icons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+		],
+		shortcut: '/icons/favicon.ico',
+		apple: [
+			{ url: '/icons/apple-touch-icon.png' },
+		],
+	},
+	manifest: '/icons/site.webmanifest',
 }
 
 export default function RootLayout({
