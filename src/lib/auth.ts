acb6d525a219a538b4b54d1e3a337c8ab79c840a
themadvlpr <themadvlpr@gmail.com>
2026-04-03 00:17:52 +0300

    fix auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,11 +3,12 @@ import { prismaAdapter } from 'better-auth/adapters/prisma'
 import prisma from './prisma'
 import { nextCookies } from 'better-auth/next-js'
 
+
 export const auth = betterAuth({
 	database: prismaAdapter(prisma, {
 		provider: 'postgresql',
 	}),
-
+	baseURL: process.env.BETTER_AUTH_URL,
 	socialProviders: {
 		github: {
 			clientId: process.env.GITHUB_CLIENT_ID as string,
