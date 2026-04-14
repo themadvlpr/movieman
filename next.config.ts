7c4dbe4a4310abac96e3147bd43a48a0d2aaf6fc
themadvlpr <themadvlpr@gmail.com>
2026-04-15 01:29:58 +0300

    FIX

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     qualities: [75, 90],
+    unoptimized: true, // Disables Vercel's image optimization service
     remotePatterns: [
       {
         protocol: 'https',
